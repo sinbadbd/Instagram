b0e1583ff8fae06d6b7136542f5419a7a72a210d
imran <imufun2@gmail.com>
2019-07-14 19:22:01 +0600

    search ui

--- Instagram/View/SearchVC.swift
+++ Instagram/View/SearchVC.swift
@@ -7,14 +7,17 @@
 //
 
 import UIKit
-class SearchVC : UICollectionViewController, UISearchBarDelegate {
+class SearchVC : UICollectionViewController, UISearchBarDelegate, UICollectionViewDelegateFlowLayout {
     
+    let SEARCH = "search_id"
     fileprivate let searchBarController = UISearchController(searchResultsController: nil)
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        collectionView.backgroundColor = .red
+        collectionView.backgroundColor = .white
         setupSearchController()
+        
+        collectionView.register(SearchCell.self, forCellWithReuseIdentifier:  SEARCH )
     }
     fileprivate func setupSearchController() {
         definesPresentationContext = true
@@ -22,6 +25,20 @@ class SearchVC : UICollectionViewController, UISearchBarDelegate {
         navigationItem.hidesSearchBarWhenScrolling = false
         searchBarController.dimsBackgroundDuringPresentation = false
         searchBarController.searchBar.delegate = self
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: SEARCH, for: indexPath) as! SearchCell
+        cell.backgroundColor = .blue
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 100)
     }
     
 }
