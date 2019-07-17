b8f8aefd1d2ac84a69547e2d53ed0e8f861ae592
imran <imufun2@gmail.com>
2019-07-17 21:02:45 +0600

    user cell ui

--- Instagram/Cell/Home/CommentPostCell.swift
+++ Instagram/Cell/Home/CommentPostCell.swift
@@ -9,9 +9,24 @@
 import UIKit
 class CommentPostCell: UICollectionViewCell{
     
+    let userCommentImg  : UIImageView = {
+        let image = UIImageView()
+        image.translatesAutoresizingMaskIntoConstraints = false
+        image.clipsToBounds = true
+        image.image = #imageLiteral(resourceName: "user")
+        image.contentMode = .scaleAspectFit
+        return image
+    }()
+    
+    
     
     override init(frame: CGRect) {
         super.init(frame: frame)
+        
+        addSubview(userCommentImg)
+        userCommentImg.image = #imageLiteral(resourceName: "user")
+        userCommentImg.anchor(top: topAnchor, leading: leadingAnchor, bottom: nil, trailing: nil, padding: .init(top: 10, left: 10, bottom: 0, right: 0), size: CGSize(width: 40, height: 40))
+        
     }
     
     required init?(coder aDecoder: NSCoder) {
